e041506553f80c187fad87928f5d441581673642
shortnd <ococncol@gmail.com>
2018-08-19 22:36:44 -0400

    changing to nuxt project

--- source/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('_layouts.master')
-
-@section('body')
-  <div class="h-screen flex flex-col items-center justify-center antialiased bg-blue-lightest">
-    <h1 class="font-serif text-4xl italic">CURRENTLY UNDER CONSTRUCTION</h1>
-    <h2 class="text-lg underline mb-2">Please Check Back in a few Days :)</h2>
-    <a class="no-underline bg-blue-lightest font-bold text-md py-4 px-6 rounded border-black border-solid border-2 text-black leading-normal hover:bg-blue hover:text-white hover:shadow" href="https://github.com/shortnd" target="_blank">Github</a>
-  </div>
-@endsection
