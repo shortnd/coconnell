d7a414b11cd6b7c3147cd8907e9c9b81e0e93399
shortnd <ococncol@gmail.com>
2018-08-19 23:19:31 -0400

    tailwind and nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,20 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
+class TailwindExtractor {
+  static extract (content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+  }
+}
+
 module.exports = {
   /*
   ** Headers of the page
   */
   head: {
-    title: '{{ name }}',
+    titleTemplate: "Collin O'Connell",
+    htmlAttrs: { lang: 'en' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -14,6 +25,17 @@ module.exports = {
     ]
   },
   /*
+  ** Load global CSS
+  */
+  css: ['@/assets/css/main.css'],
+  /*
+  ** This option is given directly to the vue-router Router constructor
+  */
+  router: {
+    base: '',
+    linkActiveClass: 'is-active'
+  },
+  /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
@@ -21,6 +43,10 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    /*
++    ** Extract CSS
++    */
+    extractCSS: true,
     /*
     ** Run ESLint on save
     */
@@ -33,7 +59,26 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+
+      /*
+      ** Cleanup CSS with PurgeCSS
+      */
+      if (!isDev) {
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            extractors: [{
+              extractor: TailwindExtractor,
+              extensions: ['vue']
+            }],
+            whitelist: ['html', 'body']
+          })
+        )
+      }
     }
   }
 }
-
